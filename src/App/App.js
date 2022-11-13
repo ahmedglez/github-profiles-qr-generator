41a6a5ac68614483fd9addf665ab2aa88f5049a9
Ahmedglez <ahmediglez@gmail.com>
2022-11-12 21:37:27 -0500

    optimizing app #1

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import '../styles/App.css';
-import Home from '../pages/Home';
-import AppContext from '../contexts/AppContext';
-import useGetUsers from '../hooks/useGetUsers';
-import UserInfo from '../pages/UserInfo';
+import React, { Suspense } from "react"
+import "../styles/App.css"
+import Home from "../pages/Home"
+import AppContext from "../contexts/AppContext"
+import useGetUsers from "../hooks/useGetUsers"
+import UserInfo from "../pages/UserInfo"
 
 const App = () => {
-	const usersApi = useGetUsers();
+	const usersApi = useGetUsers()
+	const LazyUserInfo = React.lazy(() => import("../pages/UserInfo"))
 	return (
 		<AppContext.Provider value={usersApi}>
-			<div className='App'>
+			<div className="App">
 				<Home />
-				{usersApi.showInfo &&  usersApi.valid && usersApi.user!== undefined && <UserInfo />}
+				{usersApi.showInfo && usersApi.valid && usersApi.user !== undefined && (
+					<Suspense fallback={null}>
+						<LazyUserInfo />
+					</Suspense>
+				)}
 			</div>
 		</AppContext.Provider>
-	);
-};
-export default App;
+	)
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./App/App";
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import "./styles/index.css"
+import App from "./App/App"
 
-ReactDOM.render(
-    <App />,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
